a304acb1a4a47a4c4597df1accab258790d92025
Luiz Alexandre Kades Ferrarese <107081097+luizK10@users.noreply.github.com>
2022-08-31 15:41:24 -0300

    Menus alteram cor
    Alteração dos menus

---
+++ index.js
@@ -0,0 +1,7 @@
+function alteraCorBotao(menuItem) {
+document.getElementsByClassName(menuItem)[0].style.backgroundColor = "rgb(236, 236, 236)";
+};
+
+function voltaCor (menuItem) {
+document.getElementsByClassName(menuItem)[0].style.backgroundColor = "white";
+}
