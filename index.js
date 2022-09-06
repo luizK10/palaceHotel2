45f9069cb68e81e484f6a3422fd529e73af6a129
Luiz Alexandre Kades Ferrarese <107081097+luizK10@users.noreply.github.com>
2022-09-06 12:12:05 -0300

    Cria ancora link quartos e nova janela quartos

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ document.getElementsByClassName(menuItem)[0].style.backgroundColor = "rgb(236, 2
 function voltaCor (menuItem) {
 document.getElementsByClassName(menuItem)[0].style.backgroundColor = "white";
 }
+
+function janelaQuartos (quarto) {
+    window.open(`./quartos/indexQuarto${quarto}.html`,"nome","toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,top=300,left=400,width=640,height=380");
+}
+
+
